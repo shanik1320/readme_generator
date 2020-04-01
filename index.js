024f28852581f18033739f351d836241642972e8
shanik hernandez <shaniik.her93@gmail.com>
2020-03-31 22:36:32 -0400

    made the questions

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
-
 // Require all npm packages and files
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
 
 const questions = [
-// questions to user using "enquirer"
+// questions to user using "inquirer"
+{
+    type: 'input',
+    message: "What is your GitHub username?",
+    name: "username"
+},
+{
+    type: 'input',
+    message: "What is the name of your project?",
+    name: "title"
+}, 
+{
+    type: 'input',
+    message: "How would you describe your project?",
+    name: "description"
+},
+{
+    type: 'input',
+    message: "What do you need to install your project?",
+    name: "install"
+}, 
+{
+    type: 'input',
+    message: "How would you be using the app?",
+    name: "usage"
+},    
+{
+    type: 'list',
+    message: "What license does your project need?",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "none"],
+    name: "license"
+},    
+{
+    type: 'input',
+    message: "What does a user need to know to contribute to the project?",
+    name: "contribution"
+},    
+{
+    type: 'input',
+    message: "What command should be run to test?",
+    name: "test"
+},
+{
+    type: 'input',
+    message: "How can the user use your repo?",
+    name: "repo"
+}        
 ];
 
 function init() {
+    inquirer
+    .prompt("questions")
+    .then(answers =>{ 
+        console.log(answers)
+    })
 
 // Logic for this app shoud be here. You can create more functions if you like. This is just a template for you to start your Homework but it is not required for you to use it.
 
